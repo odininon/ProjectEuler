983771bc24f2cbb3e6dc05ab08f7ade58c9cf2b9
Freyja <mike.stengel@gmail.com>
2014-06-25 14:53:12 -0400

    Question one finished

--- src/main/scala/Question1.scala
+++ src/main/scala/Question1.scala
@@ -1,6 +1,16 @@
+import scala.collection.immutable.IndexedSeq
+
 /**
  * Created by freyja on 6/25/14.
  */
-class Question1 {
+object Question1 {
+  def getSumOfMultiplesOfThreeAndFive(end: Int): Int = getMultiplesOfThreeAndFive(end).sum
+
+
+  def getMultiplesOfThreeAndFive(end: Int): IndexedSeq[Int] = {
+    val range = 1 until end
+
+    range.filter(n => n % 3 == 0 || n % 5 == 0)
+  }
 
 }
--- src/test/scala/Question1Spec.scala
+++ src/test/scala/Question1Spec.scala
@@ -6,4 +6,16 @@ import org.scalatest._
 
 class Question1Spec extends FlatSpec with Matchers {
 
+  it should "return [3,5,6,9]" in {
+    val values = Question1.getMultiplesOfThreeAndFive(10)
+
+    assert(values.equals(List(3,5,6,9)))
+  }
+
+  it should "sum up to 23" in {
+   val sum = Question1.getSumOfMultiplesOfThreeAndFive(10)
+
+   assert(sum == 23)
+  }
+
 }
