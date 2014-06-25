eec5af24c79ca7e4084a37fae1a69cdc7075ffb2
Freyja <mike.stengel@gmail.com>
2014-06-25 15:06:34 -0400

    Question two finished

--- src/main/scala/Question2.scala
+++ src/main/scala/Question2.scala
@@ -1,6 +1,12 @@
+import scala.collection.immutable.IndexedSeq
+
 /**
  * Created by freyja on 6/25/14.
  */
 object Question2 {
+
+  val fibs:Stream[BigInt] = BigInt(1) #:: BigInt(2) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2}
+
+  def getFibs(terms: Int) = fibs take terms toIndexedSeq
 
 }
--- src/test/scala/Question2Spec.scala
+++ src/test/scala/Question2Spec.scala
@@ -1,6 +1,14 @@
+import org.scalatest.{Matchers, FlatSpec}
+
 /**
  * Created by freyja on 6/25/14.
  */
-class Question2Spec {
+class Question2Spec extends FlatSpec with Matchers {
+
+  it should "return [1,2,3,5,8,13,21,34,55,89] for the first 10 terms" in {
+    val fibs = Question2.getFibs(10)
+
+    assert(fibs.equals(List(1,2,3,5,8,13,21,34,55,89)))
+  }
 
 }
